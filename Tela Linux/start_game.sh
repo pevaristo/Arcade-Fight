83c25f830b787ea0e772378a364cb9d2fb2363b4
marcelod6427 <marcelo.lima1@aluno.ifsp.edu.br>
2026-06-17 19:37:00 -0300

    Atualizacao doccker compose

--- Tela Linux/start_game.sh
+++ Tela Linux/start_game.sh
@@ -7,11 +7,12 @@ ROOT_DIR="$SCRIPT_DIR/.."
 cd "$ROOT_DIR" || exit
 
 # 1. Garante que a API e o Banco de Dados estão rodando em segundo plano
-docker compose up -d
+podman-compose up -d    
 
-# 2. Aguarda a API local ficar pronta (como é local, leva 1 ou 2 segundos)
-# O comando curl tenta acessar a API invisivelmente até ela responder
-while ! curl -s http://localhost:8000/ > /dev/null; do
+# 2. Aguarda a API local ficar pronta
+while ! curl -s http://127.0.0.1:8000/ > /dev/null; do
+    sleep 1
+done
     sleep 1
 done
 
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 echo "Iniciando o servidor do Arcade Fight..."
 
 # O parâmetro -d roda o servidor em segundo plano (detached)
-docker compose up -d
+podman-compose up -d
 
 echo "Servidor online! Iniciando a interface gráfica..."
-# Aqui você mantém o comando que abre o Electron (ex: npm start)
+python3 "Tela Linux/telaLinux.py"
