f3b2cfa703a9c30f3e50a9476a4b30d62748f798
marcelod64 <167905577+marcelod6427@users.noreply.github.com>
2026-06-17 21:09:25 -0300

    Replace podman-compose with docker compose

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 echo "Iniciando o servidor do Arcade Fight..."
 
 # O parâmetro -d roda o servidor em segundo plano (detached)
-podman-compose up -d
+docker compose up -d
 
 echo "Servidor online! Iniciando a interface gráfica..."
 python3 "Tela Linux/telaLinux.py"
